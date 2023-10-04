4db46064c120ba5dca9f00b821a16a54e39ed846
KKT <korea3702@302lab.co.kr>
2023-10-04 19:21:27 +0900

    UniTask 추가

---
+++ Assets/Scripts/Scene/Lobby.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using Fusion;
+using UnityEngine;
+
+public class Lobby : MonoBehaviour
+{
+    private NetworkRunner _runner;
+    
+    void Start()
+    {
+        _runner = NetworkManager.Instance.Runner;
+        JoinLobby();
+    }
+
+    async void JoinLobby()
+    {
+        var result = _runner.JoinSessionLobby(SessionLobby.Custom,lobbyID:"TestLobby");
+        await UniTask.WaitUntil(() => result.IsCompleted);
+    }
+    
+}
