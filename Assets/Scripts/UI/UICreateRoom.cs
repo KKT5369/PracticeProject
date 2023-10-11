600909eeb52bf63ccfc66aa2783ea7c3f4600e39
KKT <korea3702@naver.com>
2023-10-11 18:24:37 +0900

    UICreateRoom 스크립트 작업

--- Assets/Scripts/UI/UICreateRoom.cs
+++ Assets/Scripts/UI/UICreateRoom.cs
@@ -1,10 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UICreateRoom : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField inputRoomName;
+    [SerializeField] private TMP_InputField inputRoomPs;
+    [SerializeField] private TMP_Dropdown dropdownPlayerCount;
     
-    
+    [SerializeField] private Button btnCreate;
+    [SerializeField] private Button btnCancel;
+
+    private void Start()
+    {
+        SetAddListener();
+    }
+
+    void SetAddListener()
+    {
+        btnCreate.onClick.AddListener((() =>
+        {
+            print($"{inputRoomName.text}");
+            print($"{inputRoomPs.text}");
+            print($"{dropdownPlayerCount.captionText.text}");
+        }));
+    }
     
 }
