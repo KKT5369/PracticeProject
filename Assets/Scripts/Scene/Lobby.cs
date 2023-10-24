248be22465a2bd01e00e5aa87c621e53e1389bec
KKT <korea3702@302lab.co.kr>
2023-10-24 12:34:20 +0900

    네트워크 버전 추가

--- Assets/Scripts/Scene/Lobby.cs
+++ Assets/Scripts/Scene/Lobby.cs
@@ -1,16 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using Fusion;
+using Fusion.Photon.Realtime;
 using UnityEngine;
 
 public class Lobby : MonoBehaviour
 {
+    [SerializeField] private NetworkMode networkMode;
     private NetworkRunner _runner;
+    
     
     void Start()
     {
+        Init();
+    }
+
+    void Init()
+    {
+        var networkVersion = Convert.ToString($"{networkMode}_{Data.Const.NETWORKVERSION}");
+        PhotonAppSettings.Instance.AppSettings.AppVersion = networkVersion;
         _runner = NetworkManager.Instance.Runner;
         JoinLobby();
     }
